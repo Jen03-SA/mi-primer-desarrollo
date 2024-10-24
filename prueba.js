74c2d31b30f17dace2559eb4e71b11aae1144af0
Jen03-SA <jenniffersalazarestrella@gmail.com>
2024-10-24 12:19:05 -0500

    mi primer commit

---
+++ prueba.js
@@ -0,0 +1,23 @@
+// variable
+//var 
+var a = 1
+var a = 2
+console.log(a)
+//let
+
+//area del cuadrado
+var lado =5;
+var area =(lado*lado)
+console.log("el area del cuadrado es:"+ area);
+
+//area del triangulo
+let base =5;
+let altura=10;
+let resp =(base*altura)/2;
+console.log ("el area de un triangulo es;"+resp);
+g
+//area de rectangulo 
+let s = 6;
+let f =12;
+let at=(s*f);
+console.log("el area del rectangulo es:"+at)
